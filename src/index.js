2fad436a02f543837606b56dfc74d0d04e2ac380
Josey Kinnaman <josey.kinnaman@themomproject.com>
2022-08-18 10:59:12 -0700

    Remove constructor and add load function

--- src/index.js
+++ src/index.js
@@ -2,21 +2,15 @@ import React from "react";
 import  ReactDOM  from "react-dom";
 
 class App extends React.Component {
-  constructor( props ) {
-    super(props);
-    this.state = { lat: null, errorMessage: ''};
+  state = { lat: null, errorMessage: ''};
 
+  componentDidMount(){
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({lat: position.coords.latitude})
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message })
-      }
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
-  // requirement of react
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return (
